82b55df95a16c8aebd0a179bb42dfb0f344b6f99
megan-rls <megan@rlstudios.xyz>
2025-03-04 18:11:41 -0800

    backend on vercel
    backend on vercel setup

--- client/src/services/waitlistService.ts
+++ client/src/services/waitlistService.ts
@@ -2,7 +2,9 @@ import { InsertWaitlist } from '../shared/schema';
 
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api/waitlist-form'; // this should be aws/heroku/digital ocean
+// const API_URL = 'http://localhost:3000/api/waitlist-form'; // this should be aws/heroku/digital ocean
+
+const API_URL = 'https://grublify-backend.vercel.app/api/waitlist-form';
 
 const create = (newObject: InsertWaitlist) => {
   const request = axios.post(API_URL, newObject)
--- server/server.js
+++ server/server.js
@@ -39,3 +39,5 @@ app.use('/api/waitlist-form', waitlistFormRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+module.exports = app;
